eef6ee009d2f3e6c3eb7c94bc628af96d42c5af6
ElMaxh0 <83470126+ElMaxh0@users.noreply.github.com>
2021-10-16 23:24:33 -0300

    Update index.php

--- source/index.php
+++ source/index.php
@@ -3,14 +3,14 @@ $cad = $_GET['r'];
 $pesquisar = $_GET['redirect'];
 $strcon = mysqli_connect('HOST','DBUSR','PASS','ENCURTAURL_DBNAME') or die('Erro ao conectar ao banco de dados');
 echo $pesquisar;
-$result_cursos = "SELECT * FROM encurtaurl WHERE EncrtaUrl LIKE '%$pesquisar%' LIMIT 1";
-$resultado_cursos = mysqli_query($strcon, $result_cursos);
-$redirecturl =$rows_cursos['URL'];
-while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
+$result_redirect = "SELECT * FROM encurtaurl WHERE EncrtaUrl LIKE '%$pesquisar%' LIMIT 1";
+$resultado_redirect = mysqli_query($strcon, $result_redirect);
+$redirecturl =$rows_redirect['URL'];
+while($rows_redirect = mysqli_fetch_array($resultado_redirect)){
          echo "<tr>";
          echo 'Redirecionando Voce a Url :';
-		echo "<td>".$rows_cursos['URL']."<br></td>";
-        $REDIRUR = $rows_cursos['URL'];
+		echo "<td>".$rows_redirect['URL']."<br></td>";
+        $REDIRUR = $rows_redirect['URL'];
 	}
 ?>
 <meta http-equiv="refresh" content="5; URL='https://<?= $REDIRUR ?>'"/>
